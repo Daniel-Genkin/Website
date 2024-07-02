3d8f4038766463ab004260f6eb595a62a9ef1ed6
Daniel Genkin <genkind@gmail.com>
2024-07-01 20:46:13 -0700

    styled education section

---
+++ src/components/elements/StraightPill.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+withDefaults(defineProps<{
+    color: string,
+    leftRounded: boolean,
+    rightRounded: boolean,
+}>(), {
+    color: "#3A9BD2",
+    leftRounded: true,
+    rightRounded: true
+});
+</script>
+
+<template>
+  <div>
+    <div id="pill" 
+        :style="{
+            backgroundColor: color, 
+            borderTopLeftRadius: leftRounded ? '100vmax' : '0',
+            borderBottomLeftRadius: leftRounded ? '100vmax' : '0',
+            borderTopRightRadius: rightRounded ? '100vmax' : '0',
+            borderBottomRightRadius: rightRounded ? '100vmax' : '0',
+            marginLeft: leftRounded ? '24px' : '-12px',
+            marginRight: rightRounded ? '24px' : '-12px'
+        }">
+        <slot ></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#pill {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  border: 12px solid rgba(255, 255, 255, 0.25);
+  outline: 8px solid rgba(176, 176, 176, 0.25);
+  padding: 8px;
+  height: 100%;
+  width: 100%;
+  color: rgba(255, 255, 255, 0.739);
+  font-size: 1.5rem;
+}
+</style>
--- src/components/sections/Education.vue
+++ src/components/sections/Education.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import StraightPill from "@/components/elements/StraightPill.vue";
+import Chevron from "../elements/Chevron.vue";
 
 defineProps<{
   content: Education[]
@@ -7,15 +9,16 @@ defineProps<{
 
 <template>
   <div>
-    <div v-for="item in content">
-      <div class="cols">
-        <h3>{{ item.startingDate }} - {{ item.endingDate }}</h3>
+    <div class="rows">
+      <div v-for="(item, index) in content" :key="index">
+        <straight-pill class="pill" :left-rounded="index == content.length - 1">{{ item.startingDate }} - {{ item.endingDate }}</straight-pill>
         <ul>
           <li v-for="achievement in item.achievements" :key="achievement">
             {{ achievement }}
           </li>
         </ul>
       </div>
+      <chevron class="chevron"/>
     </div>
   </div>
 </template>
@@ -24,10 +27,17 @@ defineProps<{
 .rows {
   display: flex;
   flex-direction: row;
+  justify-content: left;
 }
 .cols {
   display: flex;
   flex-direction: column;
+}
+.pill {
+  margin-top: 16px;
+}
+.chevron {
+  height: 100px;
 }
 
 </style>
