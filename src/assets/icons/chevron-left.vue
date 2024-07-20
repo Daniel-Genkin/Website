f5c30a9e7813ac9ec25710b505b21895ae0d58dc
Daniel Genkin <genkind@gmail.com>
2024-07-20 08:14:53 -0700

    Fixed the rotated arrow

--- src/assets/icons/chevron-left.vue
+++ src/assets/icons/chevron-left.vue
@@ -1,5 +1,7 @@
 <template>
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" :fill="color"><path d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z" /></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" :fill="color">
+        <path d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z" />
+    </svg>
 </template>
 
 <script setup lang='ts'>
